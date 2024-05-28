bcadd1dbbbc9bf6c341d010b03dbe55af662e962
em-d3v <em_dev_9836@outlook.com>
2024-05-28 15:09:25 -0500

    added npm files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+    * @description      : Main Module
+    * @author           : em-d3v
+    * @group            : 
+    * @created          : 28/05/2024 - 15:05:43
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 28/05/2024
+    * - Author          : eVmPr
+    * - Modification    : 
+**/
+
