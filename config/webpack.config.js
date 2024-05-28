c0023cff3fd832aabda335c2e91fbc14aaee4804
em-d3v <em_dev_9836@outlook.com>
2024-05-28 16:26:26 -0500

    added more configuration files

---
+++ config/webpack.config.js
@@ -0,0 +1,20 @@
+/**
+    * @description      : 
+    * @author           : em-d3v
+    * @group            : 
+    * @created          : 28/05/2024 - 16:07:06
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 28/05/2024
+    * - Author          : eVmPr
+    * - Modification    : 
+**/
+const webpack = require('webpack');
+//production configurations
+const production = {
+
+}
+module.exports = {
+    
+}
